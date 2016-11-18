a44cd60f93d3273ff9cc2bee1b1f49176491d1ae
Tang Weizhi <tangwz.com@gmail.com>
2016-11-18 14:21:01 +0800

    A cache extension.

--- jting/local_settings.py
+++ jting/local_settings.py
@@ -6,3 +6,17 @@
 
     This is the jting's development config.
 """
+
+import datetime
+
+SITE_NAME = 'JTING'
+SITE_DESCRIPTION = 'JTing is a API-based platform.'
+SITE_YEAR = datetime.date.today().year
+
+# manifest.json location
+SITE_MANIFEST = ''
+
+# cache settings
+JTING_CACHE_TYPE = 'redis'
+JTING_CACHE_REDIS_DB = 2
+JTING_REDIS_URI = ''
