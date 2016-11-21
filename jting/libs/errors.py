e139a7ad497ef3385861ab60b6c1ac2c21a5209f
Tang Weizhi <tangwz.com@gmail.com>
2016-11-21 14:55:17 +0800

    A tmp errors.

---
+++ jting/libs/errors.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+from flask import json
+from werkzeug.exceptions import HTTPException
+from werkzeug._compat import text_type
+
+
+
+class FormError():
+    pass
+
+class NotFound():
+    pass
