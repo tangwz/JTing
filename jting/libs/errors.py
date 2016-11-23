261cb55a1e2039c5efb245447d3ab6616c74ce2d
Tang Weizhi <tangwz.com@gmail.com>
2016-11-23 15:45:38 +0800

    implement libs/errors.

--- jting/libs/errors.py
+++ jting/libs/errors.py
@@ -4,10 +4,53 @@ from flask import json
 from werkzeug.exceptions import HTTPException
 from werkzeug._compat import text_type
 
+class APIException(HTTPException):
+    code = 400
+    error = 'invalid_request'
 
+    def __init__(self, code=None, error=None, description=None, response=None):
+        if code is not None:
+            self.code = code
+        if error is not None:
+            self.error = error
+        super(APIException, self).__init__(description, response)
 
-class FormError():
-    pass
+    def get_body(self, environ=None):
+        return text_type(json.dumps(dict(
+            error = self.error,
+            error_description=self.description,
+        )))
 
-class NotFound():
-    pass
+    def get_headers(self, environ=None):
+        return [('Content-Type', 'application/json')]
+
+class FormError(APIException):
+    error = 'invalid_form'
+
+    def __init__(self, form, response=None):
+        self.form = form
+        super(FormError, self).__init__(None, response)
+
+    def get_body(self, environ=None):
+        return text_type(json.dumps(dict(
+            error = self.error,
+            error_form = self.form.errors,
+        )))
+
+class NotAuth(APIException):
+    code = 401
+    error = 'require_login'
+    description = 'Authorization is required'
+
+class NotConfidential(APIException):
+    code = 403
+    error = 'require_confidential'
+    description = 'Only confidential clients are allowed'
+
+class NotFound(APIException):
+    code = 404
+    error = 'not_found'
+
+    def __init__(self, key):
+        description = '%s not found' % key
+        super(NotFound, self).__init__(description=description)
