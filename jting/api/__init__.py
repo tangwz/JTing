6d9ce6bfa26b2a945320b2ddbdd9c6ff4a2e45cf
Tang Weizhi <tangwz.com@gmail.com>
2017-01-22 19:19:49 +0800

    add api.

---
+++ jting/api/__init__.py
@@ -0,0 +1,65 @@
+# coding: utf-8
+
+import re
+
+from flask import Blueprint, request
+
+VERSION_URL = re.compile(r'^/api/\d/')
+VERSION_ACCEPT = re.compile(r'application/vnd\.jting\+json;\s+version=(\d)')
+CURRENT_VERSION = 1
+
+bp = Blueprint('api', __name__)
+
+
+@bp.after_request
+def headers_hook(response):
+    remaining = getattr(request, '_rate_remaining', None)
+    if remaining:
+        response.headers['X-Rate-Limit'] = str(remaining)
+
+    expires = getattr(request, '_rate_expires', None)
+    if expires:
+        response.headers['X-Rate-Expires'] = str(expires)
+
+    # if request.method == 'GET':
+    response.headers['Access-Control-Allow-Origin'] = '*'
+
+    # api not available in iframe
+    response.headers['X-Frame-Options'] = 'deny'
+    # security protection
+    response.headers['Content-Security-Policy'] = "default-src 'none'"
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    return response
+
+
+def find_version(environ):
+    accept = environ.get('HTTP_ACCEPT')
+    if not accept:
+        return CURRENT_VERSION
+    rv = VERSION_ACCEPT.findall(accept)
+    if rv:
+        return rv[0]
+    return CURRENT_VERSION
+
+
+class ApiVersionMiddleware(object):
+    def __init__(self, app):
+        self.app =app
+
+    def __call__(self, environ, start_response):
+        path = environ.get('PATH_INFO')
+        if not path.startswith('/api/'):
+            return self.app(environ, start_response)
+        if VERSION_URL.match(path):
+            return self.app(environ, start_response)
+
+        version = find_version(environ)
+        environ['PATH_INFO'] = path.replace('/api/', '/api/%s/' % version)
+        return self.app(environ, start_response)
+
+
+def init_app(app):
+    app.wsgi_app = ApiVersionMiddleware(app.wsgi_app)
+
+
+    app.register_blueprint(bp, url_prefix='/api/1')
---
+++ jting/api/base.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+from functools import wraps
+from flask import request, session
+
+
+class ApiBlueprint(object):
+    def __init__(self, name):
+        self.name = name
+        self.deferred = []
+
+    def route(self, rule, **options):
+        def wrapper(f):
+            self.deferred.append((f, rule, options))
+            return f
+        return wrapper
+
+    def register(self, bp, url_prefix=None):
+        if url_prefix is None:
+            url_prefix = '/' + self.name
+
+        for f, rule, options in self.deferred:
+            endpoint = options.pop("endpoint", f.__name__)
+            bp.add_url_rule(url_prefix + rule, endpoint, f, **options)
+
+
+def require_login(permission=None):
+    def wrapper(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            pass
+        return decorated
+    return wrapper
